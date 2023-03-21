8f3b1c9107c3db0778eee3723780dfb41390f190
Yinson Tso <yinson.tso@gmail.com>
2023-03-21 16:06:47 -0400

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+upperCaseCustomerName = () => {
+    return customerName = customerName.toUpperCase()
+}
+
+setBestCustomer = () => {
+    bestCustomer = 'not bob'
+}
+
+overwriteBestCustomer = () => {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer  = 'Andrew'
+changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'George'
+}
